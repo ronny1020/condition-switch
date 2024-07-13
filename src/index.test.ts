7a23ff4b906cce2ae0901929a4c5442be1553370
ronny1020 <1020ronny@gmail.com>
2024-07-13 18:12:44 +0800

    test: fix text api

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,99 +1,112 @@
-import { describe, test } from 'vitest'
-import conditionSwitch, { ConditionWithValue } from './'
+import { describe, expect, test } from 'vitest'
+import conditionSwitch from '.'
 
 describe('conditionSwitch', () => {
   test('conditionSwitch - object condition (true)', () => {
-    const conditions: ConditionWithValue<number>[] = [
-      {
-        condition: true,
-        value: 42
-      },
-      {
-        condition: false,
-        value: 0
-      }
-    ]
+    const result = conditionSwitch(
+      [
+        {
+          condition: true,
+          value: 42
+        },
+        {
+          condition: false,
+          value: 0
+        }
+      ],
+      100
+    )
 
-    const result = conditionSwitch(conditions, 100)
-    return result === 42
+    expect(result).toBe(42)
   })
 
   test('conditionSwitch - object condition (false)', () => {
-    const conditions: ConditionWithValue<number>[] = [
-      {
-        condition: false,
-        value: 42
-      },
-      {
-        condition: true,
-        value: 0
-      }
-    ]
+    const result = conditionSwitch(
+      [
+        {
+          condition: false,
+          value: 42
+        },
+        {
+          condition: true,
+          value: 0
+        }
+      ],
+      100
+    )
 
-    const result = conditionSwitch(conditions, 100)
-    return result === 0
+    expect(result).toBe(0)
   })
 
   test('conditionSwitch - array condition (true)', () => {
-    const conditions: ConditionWithValue<number>[] = [
-      [true, 42],
-      [false, 0]
-    ]
+    const result = conditionSwitch(
+      [
+        [true, 42],
+        [false, 0]
+      ],
+      100
+    )
 
-    const result = conditionSwitch(conditions, 100)
-    return result === 42
+    expect(result).toBe(42)
   })
 
   test('conditionSwitch - array condition (false)', () => {
-    const conditions: ConditionWithValue<number>[] = [
-      [false, 42],
-      [true, 0]
-    ]
+    const result = conditionSwitch(
+      [
+        [false, 42],
+        [true, 0]
+      ],
+      100
+    )
 
-    const result = conditionSwitch(conditions, 100)
-    return result === 0
+    expect(result).toBe(0)
   })
 
   test('conditionSwitch - default value used', () => {
-    const conditions: ConditionWithValue<number>[] = [
-      [false, 42],
-      [false, 0]
-    ]
+    const result = conditionSwitch(
+      [
+        [false, 42],
+        [false, 0]
+      ],
+      100
+    )
 
-    const result = conditionSwitch(conditions, 100)
-    return result === 100
+    expect(result).toBe(100)
   })
 
   test('conditionSwitch - default value as function', () => {
-    const conditions: ConditionWithValue<number>[] = []
+    const result = conditionSwitch([], () => 100)
 
-    const result = conditionSwitch(conditions, () => 100)
-    return result === 100
+    expect(result).toBe(100)
   })
 
   test('conditionSwitch - condition as function', () => {
-    const conditions: ConditionWithValue<number>[] = [
-      [() => true, 42],
-      [() => false, 0]
-    ]
+    const result = conditionSwitch(
+      [
+        [() => true, 42],
+        [() => false, 0]
+      ],
+      100
+    )
 
-    const result = conditionSwitch(conditions, 100)
-    return result === 42
+    expect(result).toBe(42)
   })
 
   test('conditionSwitch - function as value', () => {
-    const conditions: ConditionWithValue<number>[] = [
-      {
-        condition: true,
-        value: () => 42
-      },
-      {
-        condition: false,
-        value: () => 0
-      }
-    ]
+    const result = conditionSwitch(
+      [
+        {
+          condition: true,
+          value: () => 42
+        },
+        {
+          condition: false,
+          value: () => 0
+        }
+      ],
+      100
+    )
 
-    const result = conditionSwitch(conditions, 100)
-    return result === 42
+    expect(result).toBe(42)
   })
 })
